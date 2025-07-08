553640cdab1070cacb83447666dc4f35bcc75903
cyteon <cyteon@hackclub.app>
2025-07-08 10:52:27 +0200

    licensing fixes

--- src/commands/general/source.js
+++ src/commands/general/source.js
@@ -1,35 +1,3 @@
-/*
- * DISCLAIMER:
- *
- * The source code of this project is licensed under the GNU General Public License (GPL) v3.
- *
- * IMPORTANT NOTICE REGARDING THE "source" COMMAND:
- *
- * The "source" command or functionality is an integral part of this software and MUST NOT
- * be removed, altered, or disabled in any way. This command provides users with access to
- * important project information, including the original source code and licensing details.
- *
- * The "source" command is defined as the function or method that allows users to access
- * the original source code, documentation, or the terms of the GNU General Public License
- * under which this project is distributed. It must remain in the source code, in its original
- * form, and be fully operational in any redistribution or modification of the code.
- *
- * Removing or disabling the "source" command constitutes a violation of the terms of this license.
- * You are NOT permitted to remove, modify, or otherwise tamper with this command.
- *
- * If you are redistributing or modifying this code, you must ensure that the "source" command
- * remains intact and fully functional. Failure to comply with this requirement may result in legal
- * action under the terms of the GPL v3 license.
- *
- * This software is provided "as is" without any warranty, express or implied, under the terms of
- * the GPL v3. For more information, please refer to the full GNU General Public License.
- *
- * You should have received a copy of the GNU General Public License along with this program.
- * If not, see <https://www.gnu.org/licenses/>.
- *
- * END OF DISCLAIMER.
- */
-
 import { SlashCommandBuilder } from "discord.js";
 
 const data = new SlashCommandBuilder()
@@ -50,21 +18,12 @@ const execute = async function (interaction) {
           },
           {
             name: "📜 License",
-            value: "GNU Affero General Public License v3 + extra terms",
+            value: "GNU Affero General Public License v3",
           },
           {
             name: "📜 GNU AGPL v3",
             value: "https://www.gnu.org/licenses/agpl-3.0.html",
-          },
-          {
-            name: "📜 Extra Terms",
-            value: `\`\`\`
-In addition to the terms of the GNU Affero General Public License, the following restriction applies:
-
-1. The "source" command or functionality must always be included in the source code. This command or functionality must not be removed, altered, or disabled in any way.
-   The "source" command is defined as the function or method that provides access to the originalsource code or other relevant project information.
-                        \`\`\``,
-          },
+          }
         ],
       },
     ],
